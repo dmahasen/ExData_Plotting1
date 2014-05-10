13bbe3953b69f62d93bf86c293255762b9bc68d0
dmahasen <mahasen_d@yahoo.com>
2014-05-10 09:19:01 +0530

    modification of plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 HP_con.subset.2 <- read.csv("HP_consumption_Subset.csv")
 
-png("plot2.png",width=480,height=480)
+png("plot1.png",width=480,height=480)
 hist(HP_con.subset.2$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
 dev.off()
 
